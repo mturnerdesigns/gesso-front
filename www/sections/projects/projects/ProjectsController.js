f3bf814820cd6f0f7a4fe602946087140fa08f8d
aliciabulhoes <aliciabulhoes@gmail.com>
2017-04-09 16:01:35 -0400

    Create Project Controller

---
+++ www/sections/projects/projects/ProjectsController.js
@@ -0,0 +1,28 @@
+'use strict';
+angular
+    .module('app.core')
+    .controller('ProjectsController', function($scope, $http, $routeParams, $rootScope, $route, ionicToast, $location) {
+    	$rootScope.noShow = false;
+    	$http.get('http://gesso-back.dev/api/projects').then(function(data){
+		console.log(data.data.projects);
+		$scope.projects = data.data.projects;
+
+		$scope.colors = [];
+        for( var i = 0; i<data.data.projects.length; i++){          
+          $scope.colors.push({ color: randomColor({luminosity: 'light'}) })
+      	};
+
+      	$scope.newProject = {};
+      	$scope.addProject = function(){
+      		$http.post('http://gesso-back.dev/api/projects/post', $scope.newProject,
+      		{ headers: {'X-Requested-With': 'XMLHttpRequest'}})
+      		.success(function(response){
+      			console.log(response);
+      			$route.reload();
+      		})
+      		.error(function(response){
+      			console.log(response);
+      		});
+      	};
+	});
+});

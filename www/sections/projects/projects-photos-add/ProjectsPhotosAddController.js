a48d2ad6af2eced3b578d47a18057abb40437073
aliciabulhoes <aliciabulhoes@gmail.com>
2017-04-13 20:39:54 -0400

    Move upload file function to project add page

--- www/sections/projects/projects-photos-add/ProjectsPhotosAddController.js
+++ www/sections/projects/projects-photos-add/ProjectsPhotosAddController.js
@@ -5,43 +5,5 @@ angular
     	$rootScope, $route, ionicToast, $location, ApiService, Upload) {
     	$rootScope.noShow = false;
 
-    	$scope.uploadFiles = function(file) {
-        if (file) {
-            file.upload = Upload.upload({
-                url: ApiService.URL+'project/'+$stateParams.id+'/photos/post',
-                data: {file: file}
-            })
-            .success(function(response){
-            	console.log(response);
-            	if(response.status == 422){
-            		console.log(response);
-            	} else {
-            		$state.reload();
-            	}
-            })
-
-            // file.upload.then(function (response) {
-            //     console.log(response);
-            // });
-        }   
-    }
-
-  //   	$scope.uploadFile = function(files) {
-		// 	var fileupload = new FormData();
-		// 	//Take the first selected file
-		// 	fileupload.append("file", files[0]);
-
-		// 	$http.post(ApiService.URL+'project/'+$stateParams.id+'/photos/post', fileupload, {
-		// 	    headers: {'Content-Type': 'form-data' },
-		// 	    transformRequest: angular.identity
-		// 	})
-		// 	.success(function(response){
-		// 		console.log(response);
-		// 	})
-		// 	.error(function(response){
-		// 		console.log(response);
-		// 	});
-
-		// };
-    
+   
 });

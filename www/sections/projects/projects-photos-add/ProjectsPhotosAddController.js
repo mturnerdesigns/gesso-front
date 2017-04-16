761599b82a40ef7d834a06b1295b9823a11b3132
aliciabulhoes <aliciabulhoes@gmail.com>
2017-04-16 01:09:12 -0400

    Remove "add" section from projects

--- www/sections/projects/projects-photos-add/ProjectsPhotosAddController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular
-    .module('app.core')
-    .controller('ProjectsPhotosAddController', function($scope, $http, $stateParams, 
-    	$rootScope, $route, ionicToast, $location, ApiService, Upload) {
-    	$rootScope.noShow = false;
-
-   
-});
